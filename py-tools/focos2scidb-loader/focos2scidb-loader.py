d2778ac5036ec204e366b2c6fe7f930b91e9dda8
gqueiroz <gribeiro.mg@gmail.com>
2016-04-05 08:39:41 -0300

    finishing focos2scidb-loader.py

--- py-tools/focos2scidb-loader/focos2scidb-loader.py
+++ py-tools/focos2scidb-loader/focos2scidb-loader.py
@@ -37,6 +37,11 @@ import subprocess
 scidb_cluster_name = "focos"
 monthly_start_date = "2000-01";
 daily_start_date = "2014-01-01";
+create_1d_array_cmd = "iquery -naq \"CREATE ARRAY fire_risk_1d_tmp <col_id:int16, row_id:int16, time_id:int16, v:uint16> [i=0:*,1410001];\""
+tmp_array_1d = "fire_risk_1d_tmp"
+tmp_array_data_format = "'(int16, int16, int16, uint16)'"
+array_3d_montlhy_name = "fire_risk_montlhy"
+array_3d_daily_name = "fire_risk_daily"
 
 
 def compute_monthly_time_index(year, month, initial_year, initial_month):
@@ -90,6 +95,9 @@ def extract_time_point_from_daily_data(file_name):
 
 if __name__ == '__main__':
 
+#
+# Parse command line options
+#
     parser = argparse.ArgumentParser(description="Load Fire Spot data to SciDB")
 
     required_arguments = parser.add_argument_group("Required Arguments")
@@ -97,11 +105,9 @@ if __name__ == '__main__':
     required_arguments.add_argument("-d", "--directory",
                                     help="Source directory with the fire spot data",
                                     required=True)
+
     required_arguments.add_argument("-o", "--outdir",
                                     help="Temporary directory for converting TIFF files into SciDB binary data",
-                                    required=True)
-    required_arguments.add_argument("-p", "--product",
-                                    help="The type of firespot product: 'm' for monthly or 'd' for daily",
                                     required=True)
 
     args = parser.parse_args()
@@ -110,11 +116,23 @@ if __name__ == '__main__':
 
     output_dir = args.outdir
 
-    product = args.product
-
+#
+# Search for input risk-fire files
+#
     fire_spot_files = os.popen("find {0} -name *.tif | sort".format(source_dir)).read().split('\n')
 
     fire_spot_files.remove('')
+
+#
+# For each file we have found:
+# - let's check if its name is valid
+# - let's find if it is a monthly or daily data
+# - convert it to SciDB binary format
+# - load converted data to a 1D temporary array
+# - transform and insert 1D array into final 3D array
+# - remove temporary 1D array
+#
+    print("Converting risk-fire data...")
 
     for fire_file in fire_spot_files:
 
@@ -130,14 +148,46 @@ if __name__ == '__main__':
 
         outfile_name = os.path.join(output_dir, input_file_name.replace(".tif", ".scidb"))
 
-        time_index = extract_time_point_from_monthly_data(input_file_name) if product == "m" else extract_time_point_from_daily_data(input_file_name)
+        time_index = extract_time_point_from_monthly_data(input_file_name) if match_montly else extract_time_point_from_daily_data(input_file_name)
 
         focos2scidb_cmd = "focos2scidb --f {0} --o {1} --t {2} --verbose".format(fire_file, outfile_name, time_index)
-
-        #print(focos2scidb_cmd)
 
         retcode = subprocess.call(focos2scidb_cmd, shell=True)
 
         if retcode != 0:
             print("Error converting file '{0}' to {1}.".format(fire_file, outfile_name))
             exit(1);
+
+        retcode = subprocess.call(create_1d_array_cmd, shell=True)
+
+        if retcode != 0:
+            print("Error creating temporary 1D array: '{0}'.".format(create_1d_array_cmd))
+            exit(1);
+
+        load_data_in_1d_array_cmd = "iquery -naq \"load({0}, {1}, -2, {2});\"".format(tmp_array_1d, outfile_name, tmp_array_data_format)
+
+        retcode = subprocess.call(load_data_in_1d_array_cmd, shell=True)
+
+        if retcode != 0:
+            print("Error loading file '{0}' to array 1D '{1}'.".format(outfile_name, tmp_array_1d))
+            exit(1);
+
+        array_3d = array_3d_montlhy_name if match_montly else array_3d_daily_name
+
+        load_data_in_3d_array_cmd = "iquery -naq \"insert(redimension(apply({3}, col, int64(col_id), row, int64(row_id), time_idx, int64(time_id), value, v), {1}), {0});\"".format(array_3d, array_3d, tmp_array_1d)
+
+        retcode = subprocess.call(load_data_in_3d_array_cmd, shell=True)
+
+        if retcode != 0:
+            print("Error converting 1D array '{0}' to 3D array '{1}'.".format(tmp_array_1d, array_3d))
+            exit(1);
+
+        drop_temp_1d_array_cmd = "iquery -naq \"remove({0});\"".format(tmp_array_1d)
+
+        retcode = subprocess.call(drop_temp_1d_array_cmd, shell=True)
+
+        if retcode != 0:
+            print("Error dropping 1D array '{0}'.".format(tmp_array_1d))
+            exit(1);
+
+    print("Converting risk-fire data... finished!")
